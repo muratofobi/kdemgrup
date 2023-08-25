1c74b1ad2f096d0a2abacf8fe5bba7be6cae3c02
muratofobi <62358716+muratofobi@users.noreply.github.com>
2023-08-26 01:36:34 +0300

    version01

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function() {
+    setTimeout(() => {
+        const container = document.getElementById("container");
+        container.style.opacity = 1;
+    }, 2000); // 2000 milisaniye (2 saniye) gecikme
+});
+
+
